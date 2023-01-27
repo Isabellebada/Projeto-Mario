bef05b985ddf122e43a2f53d189ef0ea83fc2be0
Isabellebada <123763856+Isabellebada@users.noreply.github.com>
2023-01-27 14:42:41 -0300

    Add files via upload

---
+++ SRC/js/index.js
@@ -0,0 +1,30 @@
+console.log('mostrar o document', document);
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+function alternarModal(){
+        modal.classList.toggle("aberto");
+
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+    
+});
+
+
+
+
+
+
+
